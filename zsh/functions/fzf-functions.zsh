a10dc8e35b4f7bda63ee0eab47ee218137ea4c6d
sigfried <rssanjuan2704@gmail.com>
2020-08-03 20:16:08 -0500

    [REF] Remove unnecesary function.

--- zsh/functions/fzf-functions.zsh
+++ zsh/functions/fzf-functions.zsh
@@ -1,14 +1,3 @@
-# unalias z 2> /dev/null # Unbind z
-# z command + fzf
-# z() {
-#   [ $# -gt 0 ] && _z "$*" && return
-#   cd "$(_z -l 2>&1 | fzf --height 40% --reverse --inline-info +s --tac --query "$*" | sed 's/^[0-9,.]* *//')"
-# }
-
-# zz() {
-#   cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
-# }
-
 # Kill process
 fkil() {
   local pid
