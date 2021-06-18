44e18fb24f818aca75748dc71d18ff05aa88fc83
sigfried <rssanjuan2704@gmail.com>
2021-06-18 09:53:47 -0500

    feat: Add new aliases & ENV variables

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias clear='clear && printf "\e[3J"'
 #alias ls='ls -lFh --sort=extension --color=auto'
 #alias lc='ls -lhC --sort=extension --color=auto'
 #Fanzy ls :)
-alias ls='exa -lFh'
+alias ls='exa -lhFs Extension'
 alias lsil='ls -l'
 alias lsc='ls -lFhC --sort=extension'
 #Configuration files.
@@ -24,4 +24,6 @@ alias ssh-remove="ssh-add -D"
 alias ssh="TERM=xterm-256color ssh"
 alias cat="bat"
 alias n="nvim"
+alias lzd="lazydocker"
+alias g="glances"
 # vim: set filetype=sh :
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -8,9 +8,11 @@ export LANGUAGE=en_US.UTF-8
 
 export GO_PATH=$PATH:/$HOME/go/bin
 export RUBY_PATH=$PATH:/$HOME/.gem/ruby/2.7.0/bin
+export PYENV_PATH="/$HOME/.pyenv/bin"
+export PYENV_BIN_PATH="/$HOME/.local/bin"
 
 export LANG=en_US.UTF-8
-export PATH=$PATH:$GO_PATH:$RUBY_PATH
+export PATH=$PYENV_PATH:$PYENV_BIN_PATH:$PATH:$GO_PATH:$RUBY_PATH
 
 #Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
@@ -18,8 +20,3 @@ if [[ -n $SSH_CONNECTION ]]; then
 else
   export EDITOR='nvim'
 fi
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
-# Colorls
-source $(dirname $(gem which colorls))/tab_complete.sh
