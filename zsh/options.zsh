91a41b886176c5b9595a6ffa5e0897755541548a
sigfried <rssanjuan2704@gmail.com>
2022-01-12 11:47:40 +0300

    feat: welcome asdf & p10k

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -33,13 +33,6 @@ setopt HIST_BEEP                 # Beep when accessing nonexistent history.
 bindkey -v
 export KEYTIMEOUT=1
 
-# vi keybinds in tab complete menu
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history-char
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history-char
-bindkey -v '^?' backward-delete-char
-
 # Edit line in vim with ctrl-e:
 bindkey '^e' edit-command-line
 
@@ -61,7 +54,7 @@ bindkey '^e' edit-command-line
 #     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
 #     echo -ne "\e[5 q"
 # }
-# 
+#
 # zle -N zle-line-init
 # echo -ne '\e[5 q' # Use beam shape cursor on startup.
 # preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
