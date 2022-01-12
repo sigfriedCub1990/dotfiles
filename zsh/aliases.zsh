3b8aa4772e31d5cc470ba7982a0071d2dbe763ed
sigfried <rssanjuan2704@gmail.com>
2022-01-12 11:45:14 +0300

    feat: Add new aliases & ENV variables

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias clear='clear && printf "\e[3J"'
 #alias ls='ls -lFh --sort=extension --color=auto'
 #alias lc='ls -lhC --sort=extension --color=auto'
 #Fanzy ls :)
-alias ls='exa -lhFs Extension'
+alias ls='exa -lhFs Extension -b'
 alias lsil='ls -l'
 alias lsc='ls -lFhC --sort=extension'
 #Configuration files.
@@ -15,9 +15,7 @@ alias vimr='vim ~/.vimrc'
 alias dev='cd $HOME/develop/'
 alias midgard='cd /media/sigfried/Midgard'
 #VirtualEnvs
-alias activate='. bin/activate'
-alias workon='virtualenv'
-alias workoff='deactivate'
+alias activate='. env/bin/activate'
 
 alias ssh-ls="ssh-add -l"
 alias ssh-remove="ssh-add -D"
@@ -26,4 +24,6 @@ alias cat="bat"
 alias n="nvim"
 alias lzd="lazydocker"
 alias g="glances"
+alias dco="docker-compose"
+alias zt="zathura"
 # vim: set filetype=sh :
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,12 +7,10 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 export GO_PATH=$PATH:/$HOME/go/bin
-export RUBY_PATH=$PATH:/$HOME/.gem/ruby/2.7.0/bin
-export PYENV_PATH="/$HOME/.pyenv/bin"
-export PYENV_BIN_PATH="/$HOME/.local/bin"
+export RUBY_PATH=$PATH:/$HOME/.gem/ruby/3.0.0/bin
 
 export LANG=en_US.UTF-8
-export PATH=$PYENV_PATH:$PYENV_BIN_PATH:$PATH:$GO_PATH:$RUBY_PATH
+export PATH=$GO_PATH:$RUBY_PATH:$PATH
 
 #Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
