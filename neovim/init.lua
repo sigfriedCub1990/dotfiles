147b987ba41aaf1546b43cb0e1ea5e8884d37823
sigfried <rssanjuan2704@gmail.com>
2022-01-14 08:38:11 +0300

    improve gutentags config

--- neovim/init.lua
+++ neovim/init.lua
@@ -184,6 +184,14 @@ vim.cmd [[
   augroup end
 ]]
 
+-- Gutentags config
+vim.g.gutentags_ctags_exclude = { '.git', '.svg', '.hg',  '/tests/', 'build', 'dist', 'bin', 'node_modules', 'bower_components', 'cache', 'compiled', 'docs', 'example', 'bundle', 'vendor', '.md', '-lock.json', '.lock', 'bundle.js', 'build.js', '.rc', '.json', '.min.', '.map', '.bak', '.zip', '.pyc', '.class', '.sln', '.Master', '.csproj', '.tmp', '.csproj.user', '.cache', '.pdb', 'tags', 'cscope.', '.css', '.less', '.scss', '.exe', '.dll', '.mp3', '.ogg', '.flac', '.swp', '.swo', '.bmp', '.gif', '.ico', '.jpg', '.png', '.rar', '.zip', '.tar', '.tar.gz', '.tar.xz', '.tar.bz2', '.pdf', '.doc', '.docx', '.ppt', '.pptx' }
+vim.g.gutentags_cache_dir = '~/.cache/nvim/tags'
+vim.g.gutentags_generate_on_write = 1
+vim.g.gutentags_generate_on_new = 1
+vim.g.gutentags_generate_on_missing = 1
+vim.g.gutentags_generate_on_empty_buffer = 0
+
 --Map blankline
 vim.g.indent_blankline_char = '┊'
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
