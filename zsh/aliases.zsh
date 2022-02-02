b77c478983b66c6eb62670292c2e203879660eb9
sigfried <rssanjuan2704@gmail.com>
2022-02-02 20:22:25 +0300

    refactor: Remove some aliases and add others

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,17 +3,11 @@ alias ..2='cd ../..'
 alias ..3='cd ../../..'
 alias ..4='cd ../../../..'
 alias clear='clear && printf "\e[3J"'
-#alias ls='ls -lFh --sort=extension --color=auto'
-#alias lc='ls -lhC --sort=extension --color=auto'
-#Fanzy ls :)
 alias ls='exa -lhFs Extension -b'
 alias lsil='ls -l'
 alias lsc='ls -lFhC --sort=extension'
 #Configuration files.
 alias vimr='vim ~/.vimrc'
-#Directories navigation
-alias dev='cd $HOME/develop/'
-alias midgard='cd /media/sigfried/Midgard'
 #VirtualEnvs
 alias activate='. env/bin/activate'
 
@@ -23,7 +17,7 @@ alias ssh="TERM=xterm-256color ssh"
 alias cat="bat"
 alias n="nvim"
 alias lzd="lazydocker"
-alias g="glances"
+alias g="git"
 alias dco="docker-compose"
 alias zt="zathura"
 # vim: set filetype=sh :
