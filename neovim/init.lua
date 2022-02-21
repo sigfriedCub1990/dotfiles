88d48d7d019f0e72bd195f54fc0ab26b4c51b530
sigfried <rssanjuan2704@gmail.com>
2022-02-21 22:31:40 +0300

    refactor: Remove unused LSP

--- neovim/init.lua
+++ neovim/init.lua
@@ -315,7 +315,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+local servers = { 'clangd', 'pyright', 'tsserver' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
