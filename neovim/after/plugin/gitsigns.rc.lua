226f85c60f6d8cdfba6c73688de377b0e85e4ac9
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-09-01 20:58:48 +0300

    Safely require Gitsigns plugin

--- neovim/after/plugin/gitsigns.rc.lua
+++ neovim/after/plugin/gitsigns.rc.lua
@@ -1,4 +1,9 @@
-require("gitsigns").setup({
+local status, gitsigns = pcall(require, "gitsigns")
+if not status then
+    return
+end
+
+gitsigns.setup({
     signs = {
         add = { hl = "GitGutterAdd", text = "+" },
         change = { hl = "GitGutterChange", text = "~" },
