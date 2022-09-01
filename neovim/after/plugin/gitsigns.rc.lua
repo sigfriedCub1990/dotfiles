4e7708bb5504ab229f8863db3f29a61342f24658
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-09-01 20:59:57 +0300

    Modify Gitsigns' signs

--- neovim/after/plugin/gitsigns.rc.lua
+++ neovim/after/plugin/gitsigns.rc.lua
@@ -5,10 +5,10 @@ end
 
 gitsigns.setup({
     signs = {
-        add = { hl = "GitGutterAdd", text = "+" },
-        change = { hl = "GitGutterChange", text = "~" },
-        delete = { hl = "GitGutterDelete", text = "_" },
-        topdelete = { hl = "GitGutterDelete", text = "‾" },
-        changedelete = { hl = "GitGutterChange", text = "~" },
+        add = { hl = "GitSignsAdd", text = "|", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "|", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
     },
 })
