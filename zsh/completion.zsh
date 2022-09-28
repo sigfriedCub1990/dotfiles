fdc3002df0bcd082eafe59684c3ccf0612599a80
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-09-28 23:17:46 +0300

    Improve ZSH completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
+
+# debug completion?
+zstyle ':completion:*' verbose yes
+
 # forces zsh to realize new commands
-zstyle ':completion:*' completer _oldlist _expand _complete _match _ignored _approximate
+zstyle ':completion:*' completer _extensions _complete _approximate
 
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
@@ -13,3 +17,15 @@ zstyle ':completion:*' rehash true
 
 # menu if nb items > 2
 zstyle ':completion:*' menu select=2
+
+# group commands by type
+zstyle ':completion:*' group-name ''
+
+# When completing files and folders list,
+# list them with all their parameters like a `ls -lh`
+zstyle ':completion:*' file-list all
+
+# highlight command descriptions and
+# corrections made to a command
+zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!-- %d (errors: %e) -!%f'
