feecacda7c72927d68afdbe5d6d76038c1580899
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-10-19 15:06:40 +0300

    fix(neovim): Neovim 8.0 breaking changes

--- neovim/plugin/lspconfig.lua
+++ neovim/plugin/lspconfig.lua
@@ -62,7 +62,7 @@ protocol.CompletionItemKind = {
 }
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local clients = { "clangd", "pyright", "elmls", "tsserver", "bashls" }
 for _, client in ipairs(clients) do
