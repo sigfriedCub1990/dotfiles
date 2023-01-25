18187051a19da65117a95a121c127dcfc8735dea
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-25 17:08:26 +0300

    feat(nvim): Add new plugins

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -5,6 +5,9 @@ if not status then
 end
 
 vim.cmd([[packadd packer.nvim]])
+vim.cmd([[
+    let g:vimtex_view_method='zathura'
+]])
 
 packer.startup(function(use)
     -- common
@@ -18,6 +21,7 @@ packer.startup(function(use)
     use("tpope/vim-fugitive")
     use("tpope/vim-commentary")
     use("tpope/vim-surround")
+    use("tpope/vim-repeat")
 
     -- UI to select things (files, grep results, open buffers...)
     use("nvim-telescope/telescope.nvim")
@@ -39,29 +43,46 @@ packer.startup(function(use)
     use("nvim-treesitter/nvim-treesitter-textobjects")
 
     -- lsp stuff
-    use("onsails/lspkind-nvim")
     use("ray-x/lsp_signature.nvim")
     use("neovim/nvim-lspconfig")
     use("williamboman/mason.nvim")
     use("williamboman/mason-lspconfig.nvim")
 
     -- autocompletion
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("saadparwaiz1/cmp_luasnip")
+    use({
+        "hrsh7th/nvim-cmp",
+        requires = {
+            -- Completion sources
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            -- Complements
+            "onsails/lspkind-nvim"
+        }
+    })
 
     -- searching
-    -- use("junegunn/fzf")
-    -- use("junegunn/fzf.vim")
+    -- Markdown
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
     -- snippets
     use("L3MON4D3/LuaSnip") -- Snippets plugin
     use("rafamadriz/friendly-snippets")
 
+    -- colorize css
+    use("norcalli/nvim-colorizer.lua")
+
+    -- csv manipulation
+    use("chrisbra/csv.vim")
+
+    -- integrate tmux & (n)vim
+    use("sunaku/tmux-navigate")
+
     -- misc
     use("jiangmiao/auto-pairs")
     use("lewis6991/gitsigns.nvim")
     use("dhruvasagar/vim-table-mode")
-    use("MunifTanjim/prettier.nvim") -- Prettier plugin for Neovim's built-in LSP client
     use("jose-elias-alvarez/null-ls.nvim")
+    use("lervag/vimtex")
 end)
