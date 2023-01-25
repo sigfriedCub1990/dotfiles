3e172318a41cf17a941964f11aa78f5445632bdc
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-25 16:59:14 +0300

    zsh: Add more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 alias ..1='cd ..'
 alias ..2='cd ../..'
 alias ..3='cd ../../..'
 alias ..4='cd ../../../..'
 alias clear='clear && printf "\e[3J"'
-alias ls='exa -lhFs Extension -b --group-directories-first'
+alias ls='exa -lhFs Extension -b --group-directories-first --git'
 alias la='exa -lahFs Extension -b --group-directories-first'
 alias lt='ls -T --git-ignore'
-alias lga='ls -G'
-alias lsil='ls -l'
-alias lsc='ls -lFhC --sort=extension'
-alias top='bpytop'
+alias lga='ls -GFs Extension -b --group-directories-first'
+alias lsil='ls -lFs Extension -b --group-directories-first'
+alias lsc='ls -lFhc --sort=extension'
+alias top='btop'
 #Configuration files.
 alias vimr='vim ~/.vimrc'
 #VirtualEnvs
@@ -30,10 +32,12 @@ alias zt="zathura"
 alias pes="pipenv shell"
 alias cx="chmod +x "
 
-# ExpressVPN aliases
-alias vpn="expressvpn connect smart"
-alias vpnc="expressvpn connect $1"
-alias vpnd="expressvpn disconnect"
-alias vpns="expressvpn status"
+# RSS reader
+alias nb="newsboat"
+
+# fasd aliases
+alias v='f -e nvim'     # quick opening files with vim
+alias m='f -e mplayer'  # quick opening files with mplayer
+alias o='a -e xdg-open' # quick opening files with xdg-open
 
 # vim: set filetype=sh :
