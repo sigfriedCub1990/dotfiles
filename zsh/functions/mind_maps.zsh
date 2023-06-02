c10546fa408e334784887d490856d75859b97d68
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-06-02 21:09:33 +0200

    feat(zsh): Improve mind maps fns

--- zsh/functions/mind_maps.zsh
+++ zsh/functions/mind_maps.zsh
@@ -3,11 +3,22 @@
 # List Mind Maps and allow you to select one
 # using fzf to filter
 function mm() {
-	if [[ $1 ]]; then
-		/usr/bin/ls --ignore="*.md" ~/mind_maps | fzf --height=50% --border=horizontal --border-label="Mind Maps" --color=label:bold:green --layout=reverse --bind "enter:execute(h-m-m ~/mind_maps/{})" --cycle --query="$1"
-	else
-		/usr/bin/ls --ignore="*.md" ~/mind_maps | fzf --height=50% --border=horizontal --border-label="Mind Maps" --color=label:bold:green --layout=reverse --bind "enter:execute(h-m-m ~/mind_maps/{})" --cycle
+	MM_DIR=~/mind_maps
+	INITIAL_QUERY=${1:-""} # Default to "" to display all files
+
+	if [[ "$1" && -e "${MM_DIR}/$1.hmm" ]]; then
+		h-m-m "$MM_DIR/$1.hmm"
 	fi
+
+	list_mind_maps | filter_results "$INITIAL_QUERY"
+}
+
+function list_mind_maps() {
+	/usr/bin/ls -R --ignore="*.md" ~/mind_maps | grep --color=never 'hmm$'
+}
+
+function filter_results() {
+	fzf --height=50% --border=horizontal --border-label="Mind Maps" --color=label:bold:green --layout=reverse --bind "enter:become(h-m-m ~/mind_maps/{})" --cycle --query=$1
 }
 
 # Creates a new Mind Map if
@@ -20,3 +31,5 @@ function cmm() {
 		mm "$1"
 	fi
 }
+
+#vim:ft=sh
