358cf3660b4d6163e541decdd6a0bd17ba8fdf03
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-08-20 20:43:23 +0200

    feat(nvim): Format on save

--- neovim/lua/rsgarxia/plugins/lsp.lua
+++ neovim/lua/rsgarxia/plugins/lsp.lua
@@ -6,6 +6,27 @@ return {
         event = "BufReadPre",
         opts = function()
           local nls = require "null-ls"
+            -- Disable tsserver formating capabilities
+            local custom_format = function(bufnr)
+                vim.lsp.buf.format({
+                    bufnr = bufnr,
+                    filter = function(client)
+                        return client.name ~= "tsserver"
+                    end,
+                    timeout_ms = 2000
+                })
+            end
+
+            local augroup_format = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
+            local format_on_save = function(bufnr)
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup_format,
+                buffer = bufnr,
+                callback = function() custom_format(bufnr) end,
+              })
+            end
+
           return {
             sources = {
               nls.builtins.formatting.prettier,
@@ -17,6 +38,12 @@ return {
               nls.builtins.formatting.ruff,
               nls.builtins.formatting.black,
             },
+            on_attach = function(client, bufnr)
+              if client.server_capabilities.documentFormattingProvider then
+                vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
+                format_on_save(bufnr)
+              end
+            end
           }
         end,
      },
