2dbee69b25c27f7404bc9025392b09b93cf87c23
Ruben S. Garcia <rgarci12@mercadona.es>
2024-06-16 00:33:44 +0200

    feat(nvim): Formatters

--- dot_config/nvim/lua/rsgarxia/plugins/formatting.lua
+++ dot_config/nvim/lua/rsgarxia/plugins/formatting.lua
@@ -1,21 +1,37 @@
+local prettierd_compatible = {
+	"typescript",
+	"typescriptreact",
+	"javascript",
+	"javascriptreact",
+	"svelte",
+	"vue",
+	"css",
+	"scss",
+	"less",
+	"html",
+	"json",
+	"jsonc",
+	"yaml",
+	"markdown",
+	"graphql",
+	"handlebars",
+}
+
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre", "BufNewFile" },
 	cmd = { "ConformInfo" },
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff_format" },
-			javascript = { { "prettierd" } },
-			typescript = { { "prettierd" } },
-			typescriptreact = { { "prettierd" } },
-			javascriptreact = { { "prettierd" } },
-			json = { { "prettierd" } },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
+	opts = function(_, opts)
+		opts.formatters_by_ft = opts.formatters_by_ft or {}
+		for _, ft in ipairs(prettierd_compatible) do
+			opts.formatters_by_ft[ft] = { "prettierd" }
+		end
+		opts.formatters_by_ft["python"] = { "ruff_format" }
+		opts.formatters_by_ft["lua"] = { "stylua" }
+
+		opts.format_on_save = {
 			timeout_ms = 1000,
 			lsp_fallback = false,
-		},
-	},
+		}
+	end,
 }
