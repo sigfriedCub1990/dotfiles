517fc3ca494eb4466bf57a52e2ba4d3742948aa4
Rubén S. García <rssanjuan2704@gmail.com>
2024-06-17 23:08:31 +0200

    feat(lsp): Add necessary LSP stuff

--- dot_config/nvim/lua/rsgarxia/plugins/init.lua
+++ dot_config/nvim/lua/rsgarxia/plugins/init.lua
@@ -27,7 +27,12 @@ return {
 			local mason_lsp = require("mason-lspconfig")
 
 			mason.setup()
-			mason_lsp.setup()
+			mason_lsp.setup({
+				ensure_installed = {
+					"tsserver",
+					"pyright",
+				},
+			})
 		end,
 	},
 	{
