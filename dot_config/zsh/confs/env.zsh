9102a2c3c1db60a957e096e7267682af53e7babd
Rubén S. García <rssanjuan2704@gmail.com>
2024-08-11 22:15:38 +0200

    refactor: Many things at once

--- dot_config/zsh/confs/env.zsh
+++ dot_config/zsh/confs/env.zsh
@@ -8,9 +8,10 @@ export LANGUAGE=en_US.UTF-8
 
 export GO_PATH=$PATH:/$HOME/go/bin
 export RUBY_PATH=$PATH:/$HOME/.gem/ruby/3.0.0/bin
+export LOCAL_SCRIPTS=$PATH:/$HOME/.local/bin
 
 export LANG=en_US.UTF-8
-export PATH=$GO_PATH:$RUBY_PATH:$PATH
+export PATH=$GO_PATH:$RUBY_PATH:$LOCAL_SCRIPTS:$PATH
 
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
