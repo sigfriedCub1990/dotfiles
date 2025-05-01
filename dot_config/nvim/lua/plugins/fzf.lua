81ea62af2ad088faf368e779d348ada10599d01c
Ruben S. Garcia <rssanjuan2704@gmail.com>
2025-05-01 13:27:23 +0200

    refactor: Improve keybind definitions

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -8,14 +8,16 @@ return {
 	},
 	opts = function(_, opts)
 		local map = require("config.utils").map
-		map("<leader><leader>", require("fzf-lua").buffers, "List open buffers")
-		map("<leader>p", require("fzf-lua").files, "Search files across project")
-		map("<leader>ss", require("fzf-lua").live_grep_native, "[S]earch [s]string across project")
-		map("<leader>sl", require("fzf-lua").live_grep_resume, "[S]earch [l]ast pattern")
-		map("<leader>sh", require("fzf-lua").helptags, "[S]earch [h]elp")
-		map("<leader>sm", require("fzf-lua").manpages, "[S]earch [m]an pages")
-		map("<leader>sw", require("fzf-lua").grep_cword, "[S]earch [w]ord under the cursor")
-		map("<leader>ca", require("fzf-lua").lsp_code_actions, "[C]ode [a]ctions")
+		local fzf_lua = require("fzf-lua")
+
+		map("<leader><leader>", fzf_lua.buffers, "List open buffers")
+		map("<leader>p", fzf_lua.files, "Search files across project")
+		map("<leader>ss", fzf_lua.live_grep_native, "[S]earch [s]string across project")
+		map("<leader>sl", fzf_lua.live_grep_resume, "[S]earch [l]ast pattern")
+		map("<leader>sh", fzf_lua.helptags, "[S]earch [h]elp")
+		map("<leader>sm", fzf_lua.manpages, "[S]earch [m]an pages")
+		map("<leader>sw", fzf_lua.grep_cword, "[S]earch [w]ord under the cursor")
+		map("<leader>ca", fzf_lua.lsp_code_actions, "[C]ode [a]ctions")
 
 		return vim.tbl_deep_extend("force", opts, {
 			fzf_opts = {
