57144e9916cfbc328f9bdac73f40293b8c6d6cb7
Ruben Garcia <rssanjuan2704@gmail.com>
2026-02-21 18:19:19 +0100

    chore: Add yazi command

--- dot_config/zsh/confs/functions/functions.zsh
+++ dot_config/zsh/confs/functions/functions.zsh
@@ -324,3 +324,11 @@ activate() {
 
     echo "Virtualenv not found, create one by running 'python -m venv venv'"
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
