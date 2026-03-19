be8aca44a698a8e2555ef5b9644b3eef6fdb9505
Ruben Garcia <rssanjuan2704@gmail.com>
2026-03-19 22:20:19 +0100

    feat(nvim): Plugin improvements

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,21 @@
 return {
 	{
 		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
 		version = "1.*",
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
 		opts = {
 			completion = { documentation = { auto_show = false } },
+			sources = {
+				-- add lazydev to your completion providers
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
+				},
+			},
 		},
 	},
 	{
